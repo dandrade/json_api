ccbe50361c357ee18408e23e7a121677434400c8
Daniel Andrade <dam@unixmexico.org>
2020-12-15 11:48:14 -0500

    Better routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      get 'cities/', to: "cities#index"
-      get 'companies/', to: "companies#index"
-      get 'people/', to: "people#index"
-      get 'people/:id', to: "people#show"
+      resources :cities, only: :index
+      resources :companies, only: :index
+      resources :people, only: [:index, :show]
+      # get 'cities/', to: "cities#index"
+      # get 'companies/', to: "companies#index"
+      # get 'people/', to: "people#index"
+      # get 'people/:id', to: "people#show"
     end
   end
 end
