742dfff71e4368c97a81d2c68637dcee4d3b7981
Daniel Andrade <dam@unixmexico.org>
2020-12-14 19:53:30 -0500

    Improves the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      get 'cities/index'
-      get 'companies/index'
-      get 'people/index'
+      get 'cities/', to: "cities#index"
+      get 'companies/', to: "companies#index"
+      get 'people/', to: "people#index"
       get 'people/:id', to: "people#show"
     end
   end
